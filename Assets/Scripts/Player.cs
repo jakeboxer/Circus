fc859aac39f89934c09b1db477b363f7684d3438
Jake Boxer <jake@github.com>
2015-01-01 15:39:09 -0800

    Get jumping working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour {
 	public float speed = 10f;
 	public Vector2 maxVelocity = new Vector2(3, 5);
 	public float airSpeedMultiplier = 0.3f;
+	public bool grounded = true;
+	public float jumpSpeed = 400f;
 	
 	private Animator animator;
 	private PlayerController controller;
@@ -21,23 +23,18 @@ public class Player : MonoBehaviour {
 		
 		var velocityX = rigidbody2D.velocity.x;
 		var absVelocityX = Mathf.Abs(velocityX);
+		var absVelocityY = Mathf.Abs(rigidbody2D.velocity.y);
 		
 		if (controller.moving.x == 0) {
 			// We're not moving horizontally.
 			animator.SetInteger("AnimState", 0);
-			
-			// Stop right away for tightness
-			rigidbody2D.velocity = new Vector2(0f, rigidbody2D.velocity.y);
 		} else {
 			// We're moving horizontally.
 			if (absVelocityX < maxVelocity.x) {
-				if ((controller.moving.x > 0f && velocityX < 0f) || (controller.moving.x < 0f && velocityX > 0f)) {
-					// We're changing direction.
-					rigidbody2D.velocity = new Vector2(0f, rigidbody2D.velocity.y);
-				} else {
-					// We haven't yet reached the horizontal speed limit.
-					forceX = speed * controller.moving.x;
-				}
+//				if ((controller.moving.x > 0f && velocityX < 0f) || (controller.moving.x < 0f && velocityX > 0f)) {
+
+				// We haven't yet reached the horizontal speed limit.
+				forceX = speed * controller.moving.x;
 				
 				// Face in the direction we're moving horizontally.
 				transform.localScale = new Vector3(controller.moving.x > 0 ? 1 : -1, 1, 1);
@@ -45,25 +42,27 @@ public class Player : MonoBehaviour {
 			
 			animator.SetInteger("AnimState", 1);
 		}
-		
-//		if (controller.moving.y > 0) { 
-//			// We're jumping.
-//			if (!jumping) {
-//				forceY = jetSpeed * controller.moving.y;
-//				jumping = true;
-//				// TODO get jumping working (right now it doesn't)
-//			}
-//			
-//			animator.SetInteger("AnimState", 2);
-//		} else if (absVelocityY > 0) {
-//			// We're not jumping but we are in the air (probably falling).
-//			animator.SetInteger("AnimState", 3);
-//		}
+
+		if (controller.moving.y > 0) { 
+			// We're jumping.
+			if (grounded && rigidbody2D.velocity.y == 0) {
+				forceY = jumpSpeed * controller.moving.y;
+				grounded = false;
+			}
+			
+			animator.SetInteger("AnimState", 2);
+		} else if (absVelocityY > 0) {
+			// We're not jumping but we are in the air (probably falling).
+			animator.SetInteger("AnimState", 3);
+		}
 		
 		rigidbody2D.AddForce(new Vector2(forceX, forceY));
 	}
 	
 	void OnCollisionEnter2D (Collision2D target) {
+		if (target.gameObject.tag == "Ground") {
+			grounded = true;
+		}
 //		if (!standing) {
 //			var absVelocityX = Mathf.Abs(rigidbody2D.velocity.x);
 //			var absVelocityY = Mathf.Abs(rigidbody2D.velocity.y);
