7c1c0dafca33e71a603766f3adb0777b0f716970
Jake Boxer <jake@github.com>
2015-01-03 16:03:24 -0800

    Hook up main menu buttons to methods

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame () {
+		Application.LoadLevel("BasicPlatformStaging");
+	}
+
+	public void ExitGame () {
+		Application.Quit();
+	}
+}
