ed56810e6f031615472090caa4c73bda371d78a6
Jake Boxer <jake@github.com>
2015-01-03 18:22:06 -0800

    Add WinPoint script

---
+++ Assets/Scripts/WinPoint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinPoint : MonoBehaviour {
+	void OnTriggerEnter2D (Collider2D otherCollider) {
+		if (otherCollider.gameObject.tag == "Player") {
+			Debug.Log("U WIN");
+			GetComponentInChildren<ParticleSystem>().Stop();
+		}
+	}
+}
